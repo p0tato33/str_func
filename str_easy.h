9c00bc4210e7500d9369d5c267736fe744d29d7e
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-10-18 00:25:29 +0300

    Create str_easy.h

---
+++ str_easy.h
@@ -0,0 +1,25 @@
+#ifndef STR_EASY_H_INCLUDED
+#define STR_EASY_H_INCLUDED
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string itc_hello_str(string name);   // 1
+long long itc_len(string str);       // 2
+void itc_print_copy_str(string str, int number);   //3
+void itc_first_end_three(string str);
+int itc_count_char_in_str(char ch, string str);
+string itc_even_place(string str);
+double itc_percent_lower_uppercase(string str);
+string itc_reverse_str(string str);
+string itc_slice_str(string str, int start, int end);
+bool itc_equal_reverse(string str);
+string itc_cmp_str(string str1, string str2, int num);
+int itc_find_str(string str1, string str2);
+string itc_three_str(string str1, string str2, string str3);
+int itc_max_char_on_end(string str);
+
+
+#endif // STR_EASY_H_INCLUDED
